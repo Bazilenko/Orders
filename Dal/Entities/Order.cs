d90f371c57d0f4f135967a2c210410680096b156
Bazilenko <kifukvasyl@gmail.com>
2025-09-22 22:51:57 +0300

    Created Entities

---
+++ Dal/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int RestaurantId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
