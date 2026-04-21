9853a5abac4c58bf90f7e1237e5dfcb7c7964c60
Vasyl <kifukvasyl@gmail.com>
2026-04-21 23:30:12 +0300

    feat: extend restaurant creation to support addresses

--- Catalog.Bll/DTOs/Restaurant/RestaurantCreateDto.cs
+++ Catalog.Bll/DTOs/Restaurant/RestaurantCreateDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Catalog.Bll.DTOs.Address;
 
 namespace Catalog.Bll.DTOs.Restaurant
 {
@@ -12,5 +13,6 @@ namespace Catalog.Bll.DTOs.Restaurant
         public decimal Rating { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
+        public List<AddressCreateDto> Addresses { get; set; }
     }
 }
